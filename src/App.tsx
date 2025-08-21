27b6f4c7ff13c229fd4be72718c070ed0080816a
Nguyen Sam <sam.np@geekup.vn>
2025-08-22 01:12:26 +0700

    feat: add animation cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import SplashCursor from "./blocks/Animations/SplashCursor/SplashCursor";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <SplashCursor />
       <div className="bg-[white] my-0">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
